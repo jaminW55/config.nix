9860b6eca47aa9c3a1f1eeb51941d7b16fc9f708
jaminW55 <133602605+jaminW55@users.noreply.github.com>
2023-11-12 16:01:53 -0500

    Create programs.nix

---
+++ programs.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  # Special Program Enablement
+  programs.dconf.enable = true; # dconf Editor
+  programs.gamemode.enable = true; # GameMode
+  programs.partition-manager.enable = true; # Partition Manager
+  programs.fish.enable = true; # Fish Shell
+
+  # Configuration for the Steam Program
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  };
+  
+  # AAGL Related Programs ( true = enable; false = disable)
+  programs.anime-game-launcher.enable = true;
+  programs.anime-borb-launcher.enable = false;
+  programs.honkers-railway-launcher.enable = true;
+  programs.honkers-launcher.enable = false;
+  };
+
+  # ... any other program configurations ...
+  
+}
