7a18b52839d9bb9983c46e805f5601e8e8335d0c
jaminW55 <133602605+jaminW55@users.noreply.github.com>
2023-11-12 15:33:58 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -60,12 +60,6 @@ in
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
