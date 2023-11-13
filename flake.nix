e100716c43e9b310b0c50f052331d1b3d40129d4
jaminW55 <133602605+jaminW55@users.noreply.github.com>
2023-11-13 02:01:38 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,10 +4,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nur.url = "github:nix-community/NUR";
+    # nixos-hardware.url = "github:NixOS/nixos-hardware";
     # Add other flake inputs if needed, such as home-manager or custom repos
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
+  outputs = { self, nixpkgs, flake-utils, nur, ... } @ inputs: # Add: nixos-hardware for Laptop Specific Hardware Settings
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         # Configure the package set, including overlay support if necessary.
@@ -28,13 +30,16 @@
         nixosConfigurations.AMDnixOS = nixpkgs.lib.nixosSystem {
           inherit system pkgs;
           modules = [
-            # Specify the path to your NixOS modules here
+            # NixOS Module Paths
             ./hardware-configuration.nix
             ./nixos-modules/packages.nix
             ./nixos-modules/localization.nix
             ./nixos-modules/programs.nix
             ./nixos-modules/services.nix
             ./nixos-modules/users.nix
+            ({ pkgs, ... }: {
+              nixpkgs.overlays = [ nur.overlay ];
+            })
           ];
         };
       }
