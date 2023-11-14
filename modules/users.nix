a2b5c1ef7e8ea9f597b3c2367baec78bacc4658a
jaminW55 <133602605+jaminW55@users.noreply.github.com>
2023-11-14 00:23:48 -0500

    Create users.nix

---
+++ modules/users.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{ 
+  users.users.$User = {
+      isNormalUser = true;
+      description = "$User Name";
+      home = "/home/$User";
+      shell = pkgs.fish;
+      extraGroups = [ "wheel" "networkmanager" "docker" ]; # Additional groups for the user
+      # User-specific packages can also be managed here, if necessary
+      packages = with pkgs; [
+        anki
+        citra-canary
+        discord
+        japanesePackages.gazou
+        japanesePackages.tachidesk
+        kommiku
+        lutris
+        obsidian
+        protonup-qt
+        ryujinx
+        thunderbird
+        wine trucks
+        wineWowPackages.stable
+        xivlauncher
+        yuzu-mainline
+        zotero
+      ];
+    };
+
+  # Global User Settings
+  users.defaultUserShell = pkgs.fish;
+
+  # Global User Groups
+  users.groups = {
+    docker = {};
+}
