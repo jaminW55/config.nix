c51e406a7190c53bc82428e35fc78580f1249c28
jaminW55 <133602605+jaminW55@users.noreply.github.com>
2023-11-14 00:28:04 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -16,12 +16,12 @@
       aagl-gtk-on-nix.module
     ];
 
-  # Nix package manager settings -- if you will not use the flake, be certain to uncomment this section -- there is no hyprland module currently, that is flake only -- so you will need to make yourself
-  # nix.settings = { 
-  #  # Cache settings to speed up build processes and binary package downloads
-  #  substituters = [ "https://ezkea.cachix.org" "https://hyprland.cachix.org" ];
-  #  trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  #};
+  # Nix package manager settings
+   nix.settings = { 
+    # Cache settings to speed up build processes and binary package downloads
+    substituters = [ "https://ezkea.cachix.org" "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
 
   # Bootloader Configuration
   boot.loader.systemd-boot.enable = true;  # Use systemd-boot as the bootloader
@@ -34,41 +34,5 @@
   # It‘s perfectly fine and recommended to leave this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option: (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
-  # User account definitions
-  users.users = {
-    $User = {
-      isNormalUser = true;
-      description = "$User Name";
-      home = "/home/$User";
-      shell = pkgs.fish;
-      extraGroups = [ "wheel" "networkmanager" "docker" ]; # Additional groups for the user
-      # User-specific packages can also be managed here, if necessary
-      packages = with pkgs; [
-        anki
-        citra-canary
-        discord
-        japanesePackages.gazou
-        japanesePackages.tachidesk
-        kommiku
-        lutris
-        obsidian
-        protonup-qt
-        ryujinx
-        thunderbird
-        wine trucks
-        wineWowPackages.stable
-        xivlauncher
-        yuzu-mainline
-        zotero
-      ];
-    };
-
-  # Global User Settings
-  users.defaultUserShell = pkgs.fish;
-
-  # Global User Groups
-  users.groups = {
-    docker = {};
 
 }
