e8d2d7c931b45e49e07b19d5a6e5a0d8ab7f2282
jaminW55 <133602605+jaminW55@users.noreply.github.com>
2023-11-14 11:17:38 -0500

    Update programs.nix

--- modules/programs.nix
+++ modules/programs.nix
@@ -6,6 +6,8 @@
   programs.gamemode.enable = true; # GameMode
   programs.partition-manager.enable = true; # Partition Manager
   programs.fish.enable = true; # Fish Shell
+
+  # Steam Program Settings
   programs.steam = { 
     # Configuration for the Steam Program
     enable = true;
@@ -17,6 +19,13 @@
   programs.anime-borb-launcher.enable = false;
   programs.honkers-railway-launcher.enable = true;
   programs.honkers-launcher.enable = false;
+
+  # Hyprland Program Settings
+  programs.hyprland = {
+    enable = true;
+    nvidiaPatches = false; # Switch to true if using Nvidia
+    xwayland.enable = true;
+  };
 
   # ... any other program configurations ...
   
